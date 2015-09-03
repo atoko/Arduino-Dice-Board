d29cbd66ccde2e9bb77c5b334615e67ac44241fd
Atoko Cardona <pedro@evermagica.com>
2015-09-02 21:41:09 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # Arduino-Dice-Board
+Arduino Dice Switchboard
+
+
+Description
+------------
+Dice board software for use with the TM1637 7-Segment Display and the Arduino Uno. Rolls the selected dice based on the switch and displays it on screen. This uses external libraries to handle the display module.
+
+Installation
+------------
+If using the arduino IDE:
+	Copy TM1637-master to your arduino libraries, make sure that it appears as a contributed library
+
+On the circuit board: 
+	Set up the display CLK & DIO to pins 8 & 9, respectively.
+	Wire inputs to digital pins 2 through 7.
+
+Schematic 
+------------
+Coming Soon
+
+Usage 
+-------------
+Pressing a switch will display the selected dice on screen. Releasing the switch will then display a random number based on the selection.
+
+
+Thanks to avishorp for the great [library](https://github.com/avishorp/TM1637) they provided for the TM1637.
---
+++ main.ino
@@ -0,0 +1,109 @@
+#include <TM1637Display.h>
+
+#define PINDTWENTY 7
+#define PINDTWELVE 6
+#define PINDTEN 5
+#define PINDEIGHT 4
+#define PINDSIX 3
+#define PINDTFOUR 2
+
+#define PINCLK 9
+#define PINDIO 8
+
+#define NO_SELECTION 0
+#define SWITCH_DEFAULT -1
+
+TM1637Display Display;
+int Selected;
+
+const uint8_t LetterD[] = {
+  0b01011110
+};
+const uint8_t LoadingFrames[] = {
+   SEG_F,
+   SEG_A,
+   SEG_B,
+   SEG_C,
+   SEG_D,
+   SEG_E
+};
+const uint8_t BlankScreen[] = { 
+  0x00, 
+  0x00, 
+  0x00, 
+  0x00 
+};
+
+void setup() 
+{
+  Display = new TM1637Display(PINCLK, PINDIO)
+  Display.setBrightness(0x0f);
+  clearScreen();
+  
+  Selected = NO_SELECTION;
+
+  randomSeed(analogRead(0));
+}
+
+void loop() 
+{
+  requestAnimationFrame(millis());
+}
+void requestAnimationFrame(long mtime)
+{
+  int currentDice = getSelectedDice();
+  
+  //Switch status same as last frame
+  if (currentDice == Selected)
+  {
+    Display.setSegments(LetterD + 0, 1, 0);
+    Display.showNumberDec(Selected, false, (Selected > 9) ? 2 : 1, 1);
+    loadingIndicator(mtime);
+  }   
+  //Switch status changed
+  else if (currentDice != SWITCH_DEFAULT)
+  {
+    clearScreen();    
+    Selected = currentDice;
+  }
+  //Switch released and selection was made
+  else if (Selected > NO_SELECTION)
+  {
+    Display.showNumberDec(random(Selected) + 1, false, 4);
+    Selected = NO_SELECTION;     
+  }
+}
+int getSelectedDice()
+{
+  int dice = checkAndRoll(PINDTWENTY, 20);
+  dice = dice == SWITCH_DEFAULT ? checkAndRoll(PINDTWELVE, 12) : dice;
+  dice = dice == SWITCH_DEFAULT ? checkAndRoll(PINDTEN, 10) : dice;
+  dice = dice == SWITCH_DEFAULT ? checkAndRoll(PINDEIGHT, 8) : dice;
+  dice = dice == SWITCH_DEFAULT ? checkAndRoll(PINDSIX, 6) : dice;
+  dice = dice == SWITCH_DEFAULT ? checkAndRoll(PINDFOUR, 4) : dice;
+
+  return current;
+}
+int checkAndRoll(int pin, int range)
+{
+  int switchState = digitalRead(pin); 
+  if (switchState == HIGH)
+  {
+    return range;
+  }
+
+  return SWITCH_DEFAULT;
+}
+
+void clearScreen()
+{
+  Display.setSegments(BlankScreen + 0);
+}
+void loadingIndicator(long mtime)
+{
+  int frame = (mtime / (1000 / 30)) % 6;
+  Display.setSegments(LoadingFrames + frame, 1, 3);
+}
+
+
+
